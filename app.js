83e8a0fb0897f189d22e5bd299449a813bf65a92
Oliulla <razasm29@gmail.com>
2022-08-30 17:03:03 +0600

    added toggler spinner

--- app.js
+++ app.js
@@ -7,9 +7,13 @@ const loadPhones = async (searchText) => {
 
 const displayPhones = (phones) => {
     // console.log(phones)
+    // phones.length === 0 ?
+    noFoundPhone(phones);
+
 
     const phoneContainer = document.getElementById('phones-container');
     phones.forEach(phone => {
+        // console.log(phone.length)
         const {image, phone_name} = phone;
 
         const phoneCard = document.createElement('div');
@@ -26,21 +30,40 @@ const displayPhones = (phones) => {
         `
         phoneContainer.appendChild(phoneCard);
     })
+
+    // toggler spinner
+    togglerSpinner(false)
 }
 
-const searchText = () => {
+// no phone founding message
+const noFoundPhone = (phones) => {
+    const warningMsg = document.getElementById('no-phone-warning');
+    phones.length === 0 ? warningMsg.classList.remove('d-none') : warningMsg.classList.add('d-none');
+}
+
+// add toggler spinner
+const togglerSpinner = (isLoading) => {
+    const loader = document.getElementById('loader');
+    isLoading ? loader.classList.remove('d-none') : loader.classList.add('d-none');
+
+}
+
+// find text from search field
+const searchFieldText = () => {
+    togglerSpinner(true)
+
     const searchText = document.getElementById('search-field').value;
     loadPhones(searchText)
 }
 
 // add event listener on search button
 document.getElementById('search-btn').addEventListener('click', btn => {
-    searchText()
+    searchFieldText()
 })
 
 // press enter button for search
 document.getElementById('search-field').addEventListener('keypress', event => {
-    event.key === 'Enter' ? searchText() : false;
+    event.key === 'Enter' ? searchFieldText() : false;
 })
 
 // loadPhones()
